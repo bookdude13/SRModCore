b3153efe85eee3c44caf876af1ca4adfb312846b
Ryan Fredlund <rfredlund13@gmail.com>
2022-06-11 16:27:29 -0500

    Initial commit, set up as MelonMod

---
+++ CopyToSR.bat
@@ -0,0 +1,6 @@
+set MOD_NAME="SRModCore"
+
+set BUILT_DLL=".\%MOD_NAME%\%MOD_NAME%\bin\Debug\%MOD_NAME%.dll"
+set SYNTHRIDERS_MODS_DIR="C:\Program Files (x86)\Steam\steamapps\common\SynthRiders\Mods"
+
+copy %BUILT_DLL% %SYNTHRIDERS_MODS_DIR%
---
+++ PrepareRelease.bat
@@ -0,0 +1,11 @@
+
+set BUILT_VERSION="1.0.0"
+set MOD_NAME="SRModCore"
+
+set RELEASE_BUILD_DIR=".\%MOD_NAME%\%MOD_NAME%\bin\Release"
+set MAIN_DLL="%RELEASE_BUILD_DIR%\%MOD_NAME%.dll"
+
+set OUTPUT_DIR=".\build\%MOD_NAME%_v%BUILT_VERSION%"
+mkdir %OUTPUT_DIR%
+
+copy %MAIN_DLL% %OUTPUT_DIR%
