7e22d798236c95296fe559908601636add201bfd
Ryan Fredlund <rfredlund13@gmail.com>
2023-10-07 16:22:25 -0500

    Update dev script for SRModCore itself

--- dev_build_copy.bat
+++ dev_build_copy.bat
@@ -1,11 +1,10 @@
+@echo off
+
 set MOD_NAME="SRModCore"
 set SYNTHRIDERS_MODS_DIR="C:\Program Files (x86)\Steam\steamapps\common\SynthRiders\Mods"
 
-echo "Cleaning output (so failures are easier to detect)"
-rmdir /S /Q build\localdev\raw
-
 echo "Building dev configuration"
-python.exe build.py --clean -n %MOD_NAME% -o build/localdev localdev
+python.exe build.py --clean -n "%MOD_NAME%" -c Debug -o build\localdev localdev build_files.txt || goto :ERROR
 
 echo "Copying to SR directory..."
 @REM Building spits out raw file structure in build/localdev/raw
@@ -15,5 +14,4 @@ echo "Done"
 goto :EOF
 
 :ERROR
-echo "Error occurred in build script!"
-exit /b %errorlevel%
+echo "Error occurred in build script! Error code: %errorlevel%"
