1096fd5935d43fcd3fa5e2ec3ee45cef254d1b73
bookdude13 <rfredlund13@gmail.com>
2025-02-27 13:38:57 -0600

    Provide a simple platform-agnostic method to get the SynthRidersUC directory

---
+++ SRModCore/FileUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SRModCore
+{
+    public class FileUtil
+    {
+        public static string GetSynthRidersUcDir()
+        {
+            // Check if we're running on Android
+            var androidPath = "/sdcard/SynthRidersUC";
+            if (Directory.Exists(androidPath))
+            {
+                return androidPath;
+            }
+
+            // Fallback on relative to game directory
+            var pcPath = Path.GetFullPath(Path.Combine(Application.dataPath, ".."));
+            return pcPath;
+        }
+    }
+}
